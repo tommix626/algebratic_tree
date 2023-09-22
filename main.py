4257710d8806cee96f78335dae1a90023b75e6e7
xh wang <xihengwang@outlook.com>
2023-09-22 18:04:12 -0400

    using cmath and add float_error detection (layer 4 = 5.721s)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-import math
+import cmath
 import time
 
 # Press ⌃R to execute it or replace it with your code.
@@ -21,6 +21,8 @@ def parse(s, start_index):
     :return: end: end index of the start_index subtree
     :return: val:[List] or int, depending on the value of the current subtree that starts at start_index.
     """
+    if start_index==None:
+        return None,None
     if type(s) != list:
         return (s,1)
     op = s[start_index]
@@ -36,9 +38,10 @@ def parse(s, start_index):
             return val1 * val2, end2
         if op == "e":
             try:
-                return (math.pow(val1, val2)), end2
+                # print(val1**val2)
+                return (val1**val2), end2
             except:
-                pass
+                return None,None
 
     if type(val1) != list:
         val1 = [val1]
@@ -52,15 +55,27 @@ def parse(s, start_index):
     return val, end2
 
 
-if __name__ == '__main__':
-    # s = ["e","e",2,-1,3]
-    # print(parse(s,0)) # returns: (['e', 0.5, 3], 4)
+def zero_check(cnum):
+    re = cnum.real
+    im = cnum.imag
+    if(abs(cnum.real) < tolerance_2):
+        re = 0
+    if (abs(cnum.imag) < tolerance_2):
+        im = 0
+    return complex(re,im)
 
+
+if __name__ == '__main__':
+    tolerance_1 = 1e-6
+    tolerance_2 = 1e-12
+    # s = ["e","e",-1,0.5,3]
+    # print(parse(s,0)) # returns: (['e', 0.5, 3], 4)
+    # time.sleep(100)
     start_time = time.time()
 
     # Your code here
 
-    for layer in range(2):
+    for layer in range(4):
 
         tempList = []
         for i1 in range(len(ansList)):
@@ -69,6 +84,8 @@ if __name__ == '__main__':
                     op.extend(ansList[i1])
                     op.extend(ansList[i2])
                     op,_ = parse(op,0)
+                    if(_==None):
+                        continue
                     if type(op) != list:
                         op = [op]
                     tempList.append(op)
@@ -76,6 +93,8 @@ if __name__ == '__main__':
                 op.extend(ansList[i1])
                 op.extend(ansList[i2])
                 op,_ = parse(op,0)
+                if (_ == None):
+                    continue
                 if type(op) != list:
                     op = [op]
                 tempList.append(op)
@@ -84,18 +103,24 @@ if __name__ == '__main__':
                 op.extend(ansList[i2])
                 op.extend(ansList[i1])
                 op, _ = parse(op, 0)
+                if (_ == None):
+                    continue
                 if type(op) != list:
                     op = [op]
                 tempList.append(op)
-        #remove repeat prefix expression
-        seen = set()
-        no_duplicates = []
-        for item in tempList:
-            tup = tuple(item)
-            if tup not in seen:
-                seen.add(tup)
-                no_duplicates.append(item)
+        # remove repeat prefix expression
         ansList.extend(tempList)
+        no_duplicates = []
+        for item1 in ansList:
+            assert (len(item1) == 1)
+            item1[0] = zero_check(item1[0])
+            flag_duplicate = 1
+            for item2 in no_duplicates:
+                if(cmath.isclose(item1[0], item2[0], rel_tol=tolerance_1, abs_tol=tolerance_1)):
+                    flag_duplicate = 0
+            if(flag_duplicate):
+                no_duplicates.append(item1)
+        ansList = no_duplicates
 
 
 
@@ -107,3 +132,4 @@ if __name__ == '__main__':
 
     for i in ansList:
         print(i)
+
