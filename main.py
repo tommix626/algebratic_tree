85245dfa2685a3f479ac7674b9b0d7bd17df499d
xh wang <xihengwang@outlook.com>
2023-09-22 21:47:11 -0400

    detect bug: not saved according to formula complexity. Add complexity tag. Also made hashset optimization layer 15 = 0.0115s layer 23 = 3.374s

--- main.py
+++ main.py
@@ -1,102 +1,123 @@
-# This is a sample Python script.
 import cmath
 import time
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-# 1; - for (-1); + for add; * for multiplication; e for exponential.
-# use prefix
-
-ansList = []
-newList = [1, -1]
+ansList = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+ansList[1] = [1, -1]
+allElementSet = set([])  # hashset for all the values generated to detect redundancy.
 tolerance_1 = 1e-6
 tolerance_2 = 1e-12
+
+
+def limit_precision(cnum, precision=6):
+    """
+    Limit the number of effective digits for a complex number.
+    """
+    re = round(cnum.real, precision)
+    im = round(cnum.imag, precision)
+    return complex(re, im)
 
 
 ##make tree structure (dictionary)
 
 
-def parse(op, val1, val2):
+def calculate(op, val1, val2):
+    """
+    calculate and limit the precision of the result op(val1,val2)
+    """
     if op not in ("+", "*", "e"):
         raise RuntimeError
     if op == "+":
-        return val1 + val2
+        result = val1 + val2
     if op == "*":
-        return val1 * val2
+        result = val1 * val2
     if op == "e":
         try:
-            return (val1 ** val2)
+            result = (val1 ** val2)
         except:
             return None
+    return limit_precision(result)
 
 
-def zero_check(cnum):
-    re = cnum.real
-    im = cnum.imag
-    if (abs(cnum.real) < tolerance_2):
-        re = 0
-    if (abs(cnum.imag) < tolerance_2):
-        im = 0
-    return complex(re, im)
+def add_new_item(item, layer):
+    """
+    add item to the ansList and allList, if not redundant.
+    """
+    # print("test")
+    if item not in allElementSet:
+        allElementSet.add(item)
+        ansList[layer].append(item)
 
 
-def add_new_item(item1, item2):
+def combine_items(item1, item2, layer):
+    """
+    generate the composition of the two item by adding a root operation.
+    Also save to according ansList[layer] if not redundant.
+    """
     for op in ["+", "*"]:
-        result = parse(op, item1, item2)
+        result = calculate(op, item1, item2)
         if result is not None:
-            tempList.append(result)
+            add_new_item(result, layer)
 
-    result = parse("e", item1, item2)
+    result = calculate("e", item1, item2)
     if result is not None:
-        tempList.append(result)
-    result = parse("e", item2, item1)
+        add_new_item(result, layer)
+
+    result = calculate("e", item2, item1)
     if result is not None:
-        tempList.append(result)
+        add_new_item(result, layer)
 
 
 if __name__ == '__main__':
 
     start_time = time.time()
 
-    for layer in range(4):
-        tempList = []
-        for i1 in range(len(ansList)):  # item1 from the old List
-            for i2 in range(i1, len(newList)):  # item2 from the new List
-                add_new_item(ansList[i1], newList[i2])
-        # new and new
-        for i1 in range(len(newList)):  # item1 from the new List
-            for i2 in range(i1, len(newList)):  # item2 from the new List
-                add_new_item(newList[i1], newList[i2])
+    for layer in range(3, 30, 2):
+        for left_complexity in range(1, (layer + 1) // 2, 2):
+            # print(f"left_complexity = {left_complexity}")
+            for i1 in ansList[left_complexity]:
+                for i2 in ansList[layer - left_complexity -1]:
+                    combine_items(i1, i2, layer)
+        print(f"layer {layer} has len {len(ansList[layer])}")
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Time taken: {elapsed_time} seconds")
 
-        ansList.extend(newList)  # merge new element from last layer into answer.
-        # maintaining no new combination can be formed within ansList.
-
-        # remove repeat prefix expression
-        # remove repeat in tempList
-        tempList_no_repeat = []
-        for item in tempList:
-            if item not in tempList_no_repeat:
-                tempList_no_repeat.append(item)
-
-        # then add element to newList if its not in ansList.
-        # ansList.extend(tempList)
-        newList = []
-        for new_item in tempList_no_repeat:
-            new_item = zero_check(new_item)
-            flag_duplicate = 1
-            for old_item in ansList:
-                if cmath.isclose(new_item, old_item, rel_tol=tolerance_1, abs_tol=tolerance_1):
-                    flag_duplicate = 0
-                    break
-            if flag_duplicate:
-                newList.append(new_item)
-
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"Time taken: {elapsed_time} seconds")
-    print(f"length of answer = {len(ansList)}")
-    time.sleep(3)
-
-    for i in ansList:
-        print(i)
+    #     for i1 in range(len(ansList)):  # item1 from the old List
+    #         for i2 in range(i1, len(newList)):  # item2 from the new List
+    #             add_new_item(ansList[i1], newList[i2])
+    #     # new and new
+    #     for i1 in range(len(newList)):  # item1 from the new List
+    #         for i2 in range(i1, len(newList)):  # item2 from the new List
+    #             add_new_item(newList[i1], newList[i2])
+    #
+    #     ansList.extend(newList)  # merge new element from last layer into answer.
+    #     # maintaining no new combination can be formed within ansList.
+    #
+    #     # remove repeat prefix expression
+    #     # remove repeat in tempList
+    #     tempList_no_repeat = []
+    #     for item in tempList:
+    #         if item not in tempList_no_repeat:
+    #             tempList_no_repeat.append(item)
+    #
+    #     # then add element to newList if its not in ansList.
+    #     # ansList.extend(tempList)
+    #     newList = []
+    #     for new_item in tempList_no_repeat:
+    #         new_item = zero_check(new_item)
+    #         flag_duplicate = 1
+    #         for old_item in ansList:
+    #             if cmath.isclose(new_item, old_item, rel_tol=tolerance_1, abs_tol=tolerance_1):
+    #                 flag_duplicate = 0
+    #                 break
+    #         if flag_duplicate:
+    #             newList.append(new_item)
+    #
+    # end_time = time.time()
+    # elapsed_time = end_time - start_time
+    # print(f"Time taken: {elapsed_time} seconds")
+    # print(f"length of answer = {len(ansList)}")
+    # time.sleep(3)
+    #
+    # for i in ansList:
+    #     print(i)
